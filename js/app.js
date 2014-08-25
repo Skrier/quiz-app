e9ef33da15ff639f9eb48b8dad9ce42156cabcbb
Skrier <alex.lewis79@yahoo.co.uk>
2014-08-25 15:58:32 +0100

    v5

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(document).ready( function () {
 /* Global variables*/
-var total = 0;
+var total = 1;
 var totalCorrect = 0;
 
 /* Object constructor */
@@ -16,15 +16,15 @@ function questionObject (question, option1, option2, option3, option4, answer, a
 
 /* Objects */
 var question1 = new questionObject('Which of the following in the national flag of South Africa',
-	                                 '../images/zalarge.gif','../images/namibia-flag.gif','../images/swaziland-flag.gif', 
-	                                 '../images/bwlarge.gif',"../images/zalarge.gif", 1 );
+	                                 "<img class='flagImage' src='images/zalarge.gif' alt='flag' />", "<img class='flagImage' src='images/namibia-flag.gif' alt='flag'/>", "<img class='flagImage' src='images/swaziland-flag.gif' alt='flag'/>", 
+	                                 "<img class='flagImage' src='images/bwlarge.gif' alt'flag' />","<img class='flagImage' src='images/zalarge.gif' alt='flag'/>", 1 );
 
 var question2 = new questionObject('How many official languages does South Africa have', '1', '5', '8', '11', 
 	                                'South Africa has 11 official languages: Afrikaans, English, Ndebele, Northern Sotho, Sotho, Swazi, Tswana, Tsonga, Venda, Xhosa and Zulu',4 );
 
 var question3 = new questionObject('How many South Africans have won the Nobel prize','0','3','5', '10', 'South Africa has 10 Nobel prize winners', 4)
 
-var question4 = new questionObject('Can you name the famous South African tech entrepenuer','Mark Shuttleworth','Elon Musk','Roelof Botha','All the Above','All the above', 4)
+var question4 = new questionObject('Can you name the famous South African tech entrepeneur','Mark Shuttleworth','Elon Musk','Roelof Botha','All the Above','All the above', 4)
 
 var questionArray = new Array (question1,question2,question3,question4);
 
@@ -35,103 +35,125 @@ $('#frontPage').fadeOut(0);
 $('#mainPage').fadeIn(2000);
 });
 
-/*Plays the game*/
-function onLoad () {
-$('.question1').text(question1.question); 
-$('#choice1').text(question1.option1); 
-$('#choice2').text(question1.option2); 
-$('#choice3').text(question1.option3); 
-$('#choice4').text(question1.option4); 
-$('.answer').css({opacity:0});
-}
-
 function nextQ () {
-$('.question1').text(questionArray[total].question); 
-$('#choice1').text(questionArray[total].option1); 
-$('#choice2').text(questionArray[total].option2); 
-$('#choice3').text(questionArray[total].option3); 
-$('#choice4').text(questionArray[total].option4); 
-$('#qI').text(total+1);
+$('.question1').html(questionArray[total-1].question); 
+$('#choice1').html(questionArray[total-1].option1); 
+$('#choice2').html(questionArray[total-1].option2); 
+$('#choice3').html(questionArray[total-1].option3); 
+$('#choice4').html(questionArray[total-1].option4); 
+$('#qI').html(total);
+console.log(total);
 }
 
 function answerQ () {
-var animation = $('.answer').text(questionArray[total].answer);
-animation.animate({opacity:0.9},600)
+$('.answer').css({opacity:0});
+$('.answer').html(questionArray[total-1].answer);
+$('.answer').animate({opacity:1},1000);
 }
 
 function evaluate () {
-	  total += 1; 
-      scoreContainer();  
-      nextQ();
-      answerQ(); 
-      
+      scoreContainer();        
+      answerQ();
+      total += 1;  
+      nextQ();      
 }
 
 /* Shows the score */
  function scoreContainer () {
  		if (totalCorrect === 0)	{
-             $('.score').css({opacity:0});
+             $('.score').css({opacity:1});
  		  } else if (totalCorrect === 1) {
-             $('.four').animate({opacity:1},600);
+             $('.four').animate({opacity:0},1000);
  		  } else if (totalCorrect === 2) {
-             $('.three').animate({opacity:1},600);
-             $('.four').animate({opacity:1},600);
+             $('.three').animate({opacity:0},1000);
+             $('.four').animate({opacity:0},1000);
  		  } else if (totalCorrect === 3) {
- 		  	$('.two').animate({opacity:1},600);
- 		  	$('.three').animate({opacity:1},600);
- 		  	$('.four').animate({opacity:1},600);
+ 		  	$('.two').animate({opacity:0},1000);
+ 		  	$('.three').animate({opacity:0},1000);
+ 		  	$('.four').animate({opacity:0},1000);
  		  } else if (totalCorrect === 4) {
- 		  	$('.one').animate({opacity:1},600);
- 		  	$('.two').animate({opacity:1},600);
- 		  	$('.three').animate({opacity:1},600);
- 		  	$('.four').animate({opacity:1},600);		  	
+ 		  	$('.one').animate({opacity:0},1000);
+ 		  	$('.two').animate({opacity:0},1000);
+ 		  	$('.three').animate({opacity:0},1000);
+ 		  	$('.four').animate({opacity:0},1000);		  	
  		  } else {
- 		  	$('.score').css({opacity:0});
+ 		  	$('.score').css({opacity:1});
  		  };
 }
 
 /* Restarts the game*/
 var replay = function () {
-	var playAgain = confirm('You answered ' + totalCorrect + ' questions correctly.\nWould you like to play again?');
-    if (playAgain) {
-        onLoad();
-        $('.score').css({opacity:0});
-    } else {
-     $('#frontPage').fadeIn(1000);
-     $('#mainPage').fadeOut(1000);
-    }
+	var playAgain = 'You answered ' + totalCorrect  + ' questions correctly.\nWould you like to play again?';
+	$('.optionsCont').hide();
+  $('.question1').html(playAgain);
+  $('#questionNo').hide();
+  $('#answerContainer').hide();
+  $('#submitButton').hide();
+  $('#yesButton').show();
+  $('#noButton').show();
+
 }
 
-/*Submits the answer*/
+function clearRadionButton () {
+	var radioB = $('.radioB');
+	for (var x = 0; x < radioB.length; x +=1 ) {
+			if (radioB[x].type = 'radio') {
+				radioB[x].checked = false;
+			}
+		} 
+}
+
+function endGame () {
+       clearRadionButton();	
+        totalCorrect = 0;
+    	  total = 1;
+          $('.optionsCont').show();
+          $('#questionNo').show();
+          $('#answerContainer').show();
+          $('#submitButton').show();
+          $('#yesButton').hide();
+          $('#noButton').hide();
+      	  $('.answer').html(' ');    	
+          $('.score').animate({opacity:1},750);
+          onload();
+}
+
+function submitAnswer () {
+	    var radioSelection = +$('input[name="options"]:checked').val();
+        if (radioSelection === questionArray[total-1].answerValue) {
+		      totalCorrect += 1;     
+		      } 
+		      evaluate();
+		      clearRadionButton();		        
+}
+
+/*Buttons*/
 $('#submitButton').on('mousedown', function(e) {
 e.preventDefault();
-var radioSelection = $('input[name="options"]:checked').val();
-if (radioSelection === questionArray[total].answerValue) {
-      totalCorrect += 1;     
-      } 
-      evaluate(); 
-         
+if (total <= 4) {
+		    submitAnswer();
+	} else if (total > 4) {
+        replay();
+	} 
 });
 
-onLoad();  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$('#yesButton').on('mousedown', function(e) {
+e.preventDefault();
+endGame();
 });
+
+$('#noButton').on('mousedown', function(e) {
+e.preventDefault();
+endGame();
+ $('#frontPage').fadeIn(1000);
+ $('#mainPage').fadeOut(10);	
+});
+
+function onload () {
+$('.answer').css({opacity:0});
+nextQ();   
+}
+ 
+onload();
+});
+
